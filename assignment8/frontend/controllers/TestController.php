e0a820fbe413f3c8c6d1dc0d148bc341c6f8fae1
Kamonwanss <s5902041610011@email.kmutnb.ac.th>
2019-09-12 13:16:57 +0700

    assignment8

---
+++ assignment8/frontend/controllers/TestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\web\Controller;
+
+class TestController extends Controller
+{
+    public function actionIndex(){
+        $data = 'data test';
+        return $this->render('index',[
+            'data' => $data
+        ]);
+    }
+    public function actionTest(){
+        $data = 'data test';
+        return $this->render('test',[
+            'data' => $data
+        ]);
+    }
+    
+}
